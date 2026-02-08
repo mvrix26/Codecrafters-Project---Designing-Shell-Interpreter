9ad8fd9cf66cce54f0a28ba7347e7213851d2f30
mvrix26 <mariomihaila2020@gmail.com>
2026-02-08 23:31:46 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -5,24 +5,25 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-  char in[100];
 
   while(1){
     printf("$ ");
-    fgets(in, 100, stdin);
+    char in[512];
+    fgets(in, 512, stdin);
     in[strcspn(in, "\n")] = '\0';
 
-    char *ec = strstr(in, "echo");
-    if(ec)
-      if(ec == in){
-        printf("%s\n", in + 5);
-        continue;
-      }
+    char *cpy_in = malloc((strlen(in) + 1) * sizeof(char)), *cmd_tok = NULL, *save = NULL;
+    memcpy(cpy_in, in, strlen(in) + 1);
+    cmd_tok = strtok_r(cpy_in, " \t", &save);
 
-    if(!strcmp(in, "exit"))
-      break;
+    if(!strcmp(cmd_tok, "exit")) { free(cpy_in); break; }
+    else if(!strcmp(cmd_tok, "echo")) printf("%s", save);
+    else if(!strcmp(cmd_tok, "type"))
+      if(!strcmp(save, "exit") || !strcmp(save, "echo") || !strcmp(save, "type")) printf("%s is a shell builtin", save);
+      else printf("%s: not found", save);
+    else printf("%s: command not found", in);
 
-    printf("%s: command not found\n", in);
+    putchar('\n');
   }
 
   return 0;
