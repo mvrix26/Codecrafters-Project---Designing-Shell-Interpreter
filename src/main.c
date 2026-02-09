f4d2a2fe84d7487140d1dfc60d715c76c7083bdb
mvrix26 <mariomihaila2020@gmail.com>
2026-02-10 00:40:10 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,29 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  while(1){
+  while(1) {
     printf("$ ");
     char in[512];
-    fgets(in, 512, stdin);
+    if(!fgets(in, 512, stdin)) break;
     in[strcspn(in, "\n")] = '\0';
 
-    char *cpy_in = malloc((strlen(in) + 1) * sizeof(char)), *cmd_tok = NULL, *save = NULL;
-    memcpy(cpy_in, in, strlen(in) + 1);
-    cmd_tok = strtok_r(cpy_in, " \t", &save);
 
-    if(!strcmp(cmd_tok, "exit")) { free(cpy_in); break; }
-    else if(!strcmp(cmd_tok, "echo")) printf("%s", save);
-    else if(!strcmp(cmd_tok, "type"))
-      if(!strcmp(save, "exit") || !strcmp(save, "echo") || !strcmp(save, "type")) printf("%s is a shell builtin", save);
-      else printf("%s: not found", save);
-    else printf("%s: command not found", in);
+    char *save = NULL;
+    char *cpy_in = strdup(in);
+    char *cmd_tok = strtok_r(cpy_in, " \t", &save);
+    if(!cmd_tok) {
+      free(cpy_in);
+      continue;
+    }
+
+
+    if(!strcmp(cmd_tok, "exit")) {
+      free(cpy_in);
+      break;
+    }
+    else if(!strcmp(cmd_tok, "echo")) {
+      char *arg = strtok_r(NULL, "", &save);
+      if(arg) printf("%s", arg);
+    }
+    else if(!strcmp(cmd_tok, "type")) {
+      char *arg = strtok_r(NULL, " \t", &save);
+      if(!arg) {}
+      else if(!strcmp(arg, "exit") || !strcmp(arg, "echo") || !strcmp(arg, "type")) {
+        printf("%s is a shell builtin", arg);
+      }
+      else {
+        char *path = getenv("PATH");
+        char *cpy_path = strdup(path);
+        char *dir = strtok(cpy_path, ":");
+        char full_path[1024];
+        int found = 0;
+
+        while(dir) {
+          snprintf(full_path, sizeof(full_path), "%s/%s", dir, arg);
+          if(!access(full_path, X_OK)) {
+            found = 1;
+            printf("%s is %s", arg, full_path);
+            break;
+          }
+          dir = strtok(NULL, ":");
+        }
+
+        if(!found) {
+          printf("%s: command not found", arg);
+        }
+
+        free(cpy_path);
+      }
+    }
+    else {
+      printf("%s: command not found", in);
+    }
 
     putchar('\n');
+    free(cpy_in);
   }
 
   return 0;
