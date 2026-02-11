3bb5e1f7f97be7e368385223a164e930d8a2b18f
mvrix26 <mariomihaila2020@gmail.com>
2026-02-11 03:26:58 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 
 int parse(char *in, char **args) {
   int argc = 0;
-  int flag = 0;
+  int sq_flag = 0, dq_flag = 0;
   char *r = in;
 
   while(*r == ' ' || *r == '\t') r++;
@@ -17,9 +17,22 @@ int parse(char *in, char **args) {
 
   while(*r != '\0') {
     if(*r == '\'') {
-      flag = !flag;
+      if(dq_flag) {
+        *w = *r;
+        w++;
+      }
+      else
+        sq_flag = !sq_flag;
     }
-    else if((*r == ' ' || *r == '\t') && !flag) {
+    else if(*r == '\"') {
+      if(sq_flag) {
+        *w = *r;
+        w++;
+      }
+      else
+        dq_flag = !dq_flag;
+    }
+    else if((*r == ' ' || *r == '\t') && !sq_flag && !dq_flag) {
       *w = '\0';
       w++;
       while(*(r+1) == ' ' || *(r+1) == '\t')
