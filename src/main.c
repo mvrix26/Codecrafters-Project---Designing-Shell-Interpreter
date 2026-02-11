c61a290fb0e23df764eff61df73aea5bcbd36a76
mvrix26 <mariomihaila2020@gmail.com>
2026-02-11 03:03:11 +0200

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -4,6 +4,42 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+int parse(char *in, char **args) {
+  int argc = 0;
+  int flag = 0;
+  char *r = in;
+
+  while(*r == ' ' || *r == '\t') r++;
+  if(*r == '\0') return 0;
+
+  args[argc++] = r;
+  char *w = r;
+
+  while(*r != '\0') {
+    if(*r == '\'') {
+      flag = !flag;
+    }
+    else if((*r == ' ' || *r == '\t') && !flag) {
+      *w = '\0';
+      w++;
+      while(*(r+1) == ' ' || *(r+1) == '\t')
+        r++;
+      if(*(r+1) != '\0')
+        args[argc++] = w;
+    }
+    else {
+      *w = *r;
+      w++;
+    }
+    r++;
+  }
+
+  *w = '\0';
+  args[argc] = NULL;
+
+  return argc;
+}
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -14,72 +50,66 @@ int main(int argc, char *argv[]) {
     if(!fgets(in, 512, stdin)) break;
     in[strcspn(in, "\n")] = '\0';
 
-    char *save = NULL;
-    char *cpy_in = strdup(in);
-    char *cmd_tok = strtok_r(cpy_in, " \t", &save);
-    if(!cmd_tok) {
-      free(cpy_in);
-      continue;
-    }
+    char *args[64];
+    int parse_cnt = parse(in, args);
+    if(!parse_cnt) continue;
 
-
-    if(!strcmp(cmd_tok, "exit")) {
-      free(cpy_in);
+    if(!strcmp(args[0], "exit")) {
       break;
     }
-    else if(!strcmp(cmd_tok, "echo")) {
-      char *arg = strtok_r(NULL, "", &save);
-      if(arg) printf("%s\n", arg);
+    else if(!strcmp(args[0], "echo")) {
+      for(int i = 1; i < parse_cnt; i++) {
+        printf("%s", args[i]);
+        if(i < parse_cnt - 1) putchar(' ');
+      }
+      putchar('\n');
     }
-    else if(!strcmp(cmd_tok, "pwd")) {
+    else if(!strcmp(args[0], "pwd")) {
       char cwd[1024];
       getcwd(cwd, sizeof(cwd));
       printf("%s\n", cwd);
     }
-    else if(!strcmp(cmd_tok, "cd")) {
-      char *arg = strtok_r(NULL, " \t", &save);
-      if(arg) {
-        if(!strcmp(arg, "~")) {
+    else if(!strcmp(args[0], "cd")) {
+      if(parse_cnt > 1) {
+        if(!strcmp(args[1], "~")) {
           char *home = getenv("HOME");
           if(home) {
             if(chdir(home))
-              printf("%s: %s: No such file or directory\n", cmd_tok, home);
+              printf("%s: %s: No such file or directory\n", args[0], home);
           }
-          else printf("%s: HOME not set\n", cmd_tok);
-
+          else printf("%s: HOME not set\n", args[0]);
         }
-        else if(chdir(arg))
-          printf("%s: %s: No such file or directory\n", cmd_tok, arg);
+        else if(chdir(args[1]))
+          printf("%s: %s: No such file or directory\n", args[0], args[1]);
       }
     }
-    else if(!strcmp(cmd_tok, "type")) {
-      char *arg = strtok_r(NULL, " \t", &save);
-      if(!arg) {}
-      else if(!strcmp(arg, "exit") || !strcmp(arg, "echo") || !strcmp(arg, "type") ||!strcmp(arg, "pwd") || !strcmp(arg, "cd")) {
-        printf("%s is a shell builtin\n", arg);
-      }
-      else {
-        char *path = getenv("PATH");
-        char *cpy_path = strdup(path);
-        char *dir = strtok(cpy_path, ":");
-        char full_path[1024];
-        int found = 0;
+    else if(!strcmp(args[0], "type")) {
+      if(parse_cnt > 1){
+        if(!strcmp(args[1], "exit") || !strcmp(args[1], "echo") || !strcmp(args[1], "type") ||!strcmp(args[1], "pwd") || !strcmp(args[1], "cd")) {
+          printf("%s is a shell builtin\n", args[1]);
+        } else {
+          char *path = getenv("PATH");
+          char *cpy_path = strdup(path);
+          char *dir = strtok(cpy_path, ":");
+          char full_path[1024];
+          int found = 0;
 
-        while(dir) {
-          snprintf(full_path, sizeof(full_path), "%s/%s", dir, arg);
-          if(!access(full_path, X_OK)) {
-            found = 1;
-            printf("%s is %s\n", arg, full_path);
-            break;
+          while(dir) {
+            snprintf(full_path, sizeof(full_path), "%s/%s", dir, args[1]);
+            if(!access(full_path, X_OK)) {
+              found = 1;
+              printf("%s is %s\n", args[1], full_path);
+              break;
+            }
+            dir = strtok(NULL, ":");
           }
-          dir = strtok(NULL, ":");
-        }
 
-        if(!found) {
-          printf("%s: not found\n", arg);
-        }
+          if(!found) {
+            printf("%s: not found\n", args[1]);
+          }
 
-        free(cpy_path);
+          free(cpy_path);
+        }
       }
     }
     else {
@@ -90,22 +120,13 @@ int main(int argc, char *argv[]) {
       int found = 0;
 
       while(dir) {
-        snprintf(full_path, sizeof(full_path), "%s/%s", dir, cmd_tok);
+        snprintf(full_path, sizeof(full_path), "%s/%s", dir, args[0]);
         if(!access(full_path, X_OK)){
-          char *args[64] = {NULL};
-          char *arg = strtok_r(NULL, " \t", &save);
-          args[0] = cmd_tok;
-          int i = 1;
-          while(arg) {
-            args[i++] = arg;
-            arg = strtok_r(NULL, " \t", &save);
-          }
-          args[i] = NULL;
-
           found = 1;
           pid_t pid = fork();
           if(!pid){
             execv(full_path, args);
+            exit(1);
           }
           else {
             wait(NULL);
@@ -117,11 +138,9 @@ int main(int argc, char *argv[]) {
 
       free(cpy_path);
       if(!found){
-        printf("%s: command not found\n", cmd_tok);
+        printf("%s: command not found\n", args[0]);
       }
     }
-
-    free(cpy_in);
   }
 
   return 0;
