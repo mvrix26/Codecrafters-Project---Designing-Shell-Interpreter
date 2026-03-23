99ef779a0ad124bad0a0b748e992877ae321bec7
mvrix26 <mariomihaila2020@gmail.com>
2026-03-24 01:56:14 +0200

    Added backslash feature outside of ''/ + much more visible code inside of builtins.c file

--- src/parser.c
+++ src/parser.c
@@ -6,38 +6,39 @@ int parse(char *in, char **args) {
     int sq_flag = 0, dq_flag = 0;
     char *r = in;
 
-    while(*r == ' ' || *r == '\t') r++;
-    if(*r == '\0') return 0;
+    while (*r == ' ' || *r == '\t') r++;
+    if (*r == '\0') return 0;
 
     args[argc++] = r;
     char *w = r;
 
-    while(*r != '\0') {
-        if(*r == '\'') {
-            if(dq_flag) {
+    while (*r != '\0') {
+        if (*r == '\'') {
+            if (dq_flag) {
                 *w = *r;
                 w++;
-            }
-            else
+            } else
                 sq_flag = !sq_flag;
-        }
-        else if(*r == '\"') {
-            if(sq_flag) {
+        } else if (*r == '\"') {
+            if (sq_flag) {
                 *w = *r;
                 w++;
-            }
-            else
+            } else
                 dq_flag = !dq_flag;
-        }
-        else if((*r == ' ' || *r == '\t') && !sq_flag && !dq_flag) {
+        } else if ((*r == ' ' || *r == '\t') && !sq_flag && !dq_flag) {
             *w = '\0';
             w++;
-            while(*(r+1) == ' ' || *(r+1) == '\t')
+            while (*(r + 1) == ' ' || *(r + 1) == '\t')
                 r++;
-            if(*(r+1) != '\0')
+            if (*(r + 1) != '\0')
                 args[argc++] = w;
-        }
-        else {
+        } else if (*r == '\\' && !sq_flag && !dq_flag) {
+            if (*(r + 1) == '\0')
+                break;
+            *w = *(r + 1);
+            w++;
+            r++;
+        } else {
             *w = *r;
             w++;
         }
