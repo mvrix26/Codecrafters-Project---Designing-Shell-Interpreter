d7cd967b49a74f555c0729218b377eafff091af5
mvrix26 <mariomihaila2020@gmail.com>
2026-03-23 23:38:53 +0200

    Modified code formatation

--- src/main.c
+++ src/main.c
@@ -6,25 +6,25 @@
 #include "executor.h"
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+    // Flush after every printf
+    setbuf(stdout, NULL);
 
-  while(1) {
-    printf("$ ");
-    char in[512];
-    if(!fgets(in, 512, stdin)) break;
-    in[strcspn(in, "\n")] = '\0';
+    while (1) {
+        printf("$ ");
+        char in[512];
+        if (!fgets(in, 512, stdin)) break;
+        in[strcspn(in, "\n")] = '\0';
 
-    char *args[64];
-    int parse_cnt = parse(in, args);
-    if(!parse_cnt) continue;
+        char *args[64];
+        int parse_cnt = parse(in, args);
+        if (!parse_cnt) continue;
 
-    if (is_builtin(args[0])) {
-      exec_builtin(args, parse_cnt);
-    } else {
-      exec_external(args);
+        if (is_builtin(args[0])) {
+            exec_builtin(args, parse_cnt);
+        } else {
+            exec_external(args);
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
